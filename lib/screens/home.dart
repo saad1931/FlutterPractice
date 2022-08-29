2fb3ac33e2178e122d41445954463ddaa92ba333
Muhammad Saad <muhammadsaad3119@gmail.com>
2022-08-30 00:13:58 +0500

    day 9 completed

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_challenge/models/catalog.dart';
+import 'package:flutter_challenge/widgets/ItemWidget.dart';
 import 'package:flutter_challenge/widgets/drawer.dart';
 
 class Home extends StatelessWidget {
@@ -6,14 +8,19 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final dummylist=List.generate(10,(index) => CatalogModel.items[0]);
     return Scaffold(
        appBar: AppBar(
         centerTitle: true,
         title:Text("Catalog App",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),)
        ),
-       body: Center(
-         child: Container(
-          child: Text("Welcome to the app"),
+       body: Padding(
+         padding: const EdgeInsets.all(16.0),
+         child: ListView.builder(
+          itemCount: dummylist.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(item: dummylist[index]);
+          },
          ),
        ),
        drawer: MyDrawer(),
